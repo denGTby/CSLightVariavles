c07663ebd07938373743ebf6247f3a2576ef0b2a
dengt_by dengt_by <denis.wse.new@gmail.com>
2023-04-10 14:44:51 +0300

    Добавьте файлы проекта.

---
+++ CSLightVariavles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLightVariavles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte singleBuferRX = 0;
+            ushort digitalTemperature = 65535;
+            int maxVoltageAC = 2000000;
+            uint countPeopleSubwayPerDay;
+            ulong peopleCountry = 130000000;
+            float temperatureСelsius = -15.3f;
+            double radiusSolarSystem = 15e-12;
+            char signMoney = '$';
+            string nameMoney = "рубль";
+            bool isToday;
+        }
+    }
+}
